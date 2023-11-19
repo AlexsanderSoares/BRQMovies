738e4fdcd93006f88ccabe2313ddcdb389dc0bf5
Alexsander Soares <alexsander.silva@wisereducacao.com>
2023-11-19 14:16:57 -0300

    fix: focus in input when touch label in login screen

--- src/components/FloatingLabelInput/component.tsx
+++ src/components/FloatingLabelInput/component.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   useAnimatedStyle, useSharedValue, withTiming,
 } from 'react-native-reanimated';
 import {Container, Input, InputContainer, Label} from './styles';
 import { ComponentProps } from './types';
+import { TextInput } from 'react-native';
 
 const FloatingLabelInput = ({label, LeftIcon, RightIcon, error, ...props}: ComponentProps) => {
   const isFocused = useSharedValue(false);
+
+  const inputRef = useRef<TextInput | null>(null)
 
   const labelStyle = useAnimatedStyle(() => {
     return {
@@ -27,9 +30,10 @@ const FloatingLabelInput = ({label, LeftIcon, RightIcon, error, ...props}: Compo
     <Container error={error} testID={error ? 'floating-label-input-container-error' : 'floating-label-input-container'}>
       {LeftIcon && <LeftIcon />}
       <InputContainer>
-        <Label style={labelStyle}>{label}</Label>
+        <Label onPress={() => inputRef?.current?.focus()} style={labelStyle}>{label}</Label>
         <Input
           {...props}
+          ref={inputRef}
           onFocus={handleFocus}
           selectionColor={"#fff"}
           onBlur={handleBlur}
