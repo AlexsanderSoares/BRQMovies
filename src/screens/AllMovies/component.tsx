8d329666f3312f8cbd6ad30d26a474583923b6fc
Alexsander Soares <alexsander.silva@wisereducacao.com>
2023-11-19 13:29:49 -0300

    feat: messages in all movies screen and refresh control

--- src/screens/AllMovies/component.tsx
+++ src/screens/AllMovies/component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FlashList } from "@shopify/flash-list";
 
-import { Container } from './styles';
+import { Container, EmptyListComponent, Message, RetryButton, RetryButtonText } from './styles';
 import { MovieCard } from '../../components/MovieCard';
 import { Movie } from './types';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
@@ -13,25 +13,30 @@ import { RootStackParamList } from '../../routes/types';
 const AllMovies: React.FC = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>()
 
-  const { data: movies = [], isLoading, isError, error } = usePopularMovies();
+  const { data: movies = [], isLoading, isError, refetch } = usePopularMovies();
 
   if (isLoading) {
     return (
-      <Container>
-        <Text>
-          Carregando...
-        </Text>
-      </Container>
+      <EmptyListComponent>
+        <Message>
+            Carregando...
+        </Message>
+      </EmptyListComponent>
     );
   }
 
   if (isError) {
     return (
-      <Container>
-        <Text>
-          Ocorreu um erro
-        </Text>
-      </Container>
+      <EmptyListComponent>
+        <Message>
+            Ops... Ocorreu um erro
+        </Message>
+        <RetryButton onPress={() => refetch()}>
+          <RetryButtonText>
+            Tente novamente
+          </RetryButtonText>
+        </RetryButton>
+      </EmptyListComponent>
     )
   }
 
@@ -47,6 +52,15 @@ const AllMovies: React.FC = () => {
         renderItem={renderItem}
         numColumns={2}
         estimatedItemSize={100}
+        ListEmptyComponent={(
+          <EmptyListComponent>
+            <Message>
+              Nenhum filme foi encontrado
+            </Message>
+          </EmptyListComponent>
+        )}
+        refreshing={isLoading}
+        onRefresh={refetch}
       />
     </Container>
   );
--- src/screens/AllMovies/styles.ts
+++ src/screens/AllMovies/styles.ts
@@ -7,6 +7,34 @@ export const Container = styled.View`
     padding-left: 8px;
     padding-right: 8px;
     padding-bottom: 24px;
-    justify-content: center;
     flex-direction: row;
+`;
+
+export const EmptyListComponent = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: #16171B;
+`;
+
+export const Message = styled.Text`
+    font-size: 25px;
+    font-weight: bold;
+    color: #fff;
+`;
+
+export const RetryButton = styled.TouchableOpacity`
+    width: 200px;
+    height: 50px;
+    justify-content: center;
+    align-items: center;
+    background-color: #EC8B00;
+    border-radius: 8px;
+    margin-top: 15px;
+`;
+
+export const RetryButtonText = styled.Text`
+    font-size: 20px;
+    font-weight: 700;
+    color: #fff;
 `;
