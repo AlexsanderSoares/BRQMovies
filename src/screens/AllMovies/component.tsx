2580b40a7dce77687661e38c760220ceaff74476
Alexsander Soares <alexsander.silva@wisereducacao.com>
2023-11-19 16:12:21 -0300

    feat: connection error message in all movies screen

--- src/screens/AllMovies/component.tsx
+++ src/screens/AllMovies/component.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { FlashList } from "@shopify/flash-list";
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import {useNetInfo} from '@react-native-community/netinfo';
 
 import { Container, EmptyListComponent, Message, RetryButton, RetryButtonText } from './styles';
 import { MovieCard } from '../../components/MovieCard';
 import { Movie } from './types';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
 import usePopularMovies from './component.hook';
-import { Text } from 'react-native';
 import { RootStackParamList } from '../../routes/types';
 
 
 const AllMovies: React.FC = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>()
-
+  const netInfo = useNetInfo()
   const { data: movies = [], isLoading, isError, refetch } = usePopularMovies();
+
+  if (!netInfo.isConnected) {
+    return (
+      <EmptyListComponent>
+        <Message>
+            Sem conexão com a internet
+        </Message>
+        <RetryButton onPress={() => refetch()}>
+          <RetryButtonText>
+            Tente novamente
+          </RetryButtonText>
+        </RetryButton>
+      </EmptyListComponent>
+    );
+  }
 
   if (isLoading) {
     return (
